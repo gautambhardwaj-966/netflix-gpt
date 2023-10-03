9c16dcfdc9b11b68bb736703d75b09e7e02289e9
gautambhardwaj-966 <gautambhardwaj9661@gmail.com>
2023-10-03 13:23:27 +0530

    Pre_final

--- src/components/Header.js
+++ src/components/Header.js
@@ -66,8 +66,9 @@ dispatch(changeLangage(e.target.value))
   };
   
   return (
-    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black flex justify-between">
-      <img className="w-44" src={LOGO} alt="logo"/>
+    // sm:bg-blue-900 md:bg-green-900 for small devices it will be blue for medium devices it will be green 
+    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row md:justify-between">
+      <img className="w-44 mx-auto" src={LOGO} alt="logo"/>
 
       {user && (
       <div className="flex p-2">
